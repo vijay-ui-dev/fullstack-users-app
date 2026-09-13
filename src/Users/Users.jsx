7a1cd65ca02693386b86591850dd72d64a803701
Vijay Kumar <mvijay0687@gmail.com>
2026-09-13 17:54:39 +0530

    api url changed

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -7,7 +7,7 @@ function Users( {onUserAdded } ){
     
     const addUser= async ()=>{
         if(userName.trim()==="")return;
-         await fetch("https://fullstack-backend-app.onrender.com//users", {
+         await fetch("https://fullstack-backend-app.onrender.com/users", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name: userName, age: 0, city: "" })
